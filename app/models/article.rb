99f2cb93dae6693f13936be6011ad66edb61d5ed
lucas-ideapad <lcppcoding@gmail.com>
2021-08-08 00:01:23 -0300

    first inclusion successful

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   enum source: ['Cultura', 'Desenvolvimento Social']
   validates :url, :title, :publish_date, :collect_date, :content, :source, presence: true
-  validates :url, uniqueness: true
+  validates :url, uniqueness: { message: 'already persisted' }
 end
